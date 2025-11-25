afb8f07e4721e7a5b0d1dadf83f841961865b362
maotto <maotto@taltech.ee>
2025-11-25 17:36:13 +0200

    Can set wallpaper using daemon

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # wallpaper-engine-plasma
 
 Wallpaper engine for KDE Plasma.
+
+### To-do
+- [ ] Check if path is dir or file (two arguments for single image and dir)
+- [ ] Check if path has ".." or " ' "
+- [ ] Run in background
+- [ ] Period argument
+- [ ] Config file
+- [ ] Per-monitor wallpapers (default to all or main)
+- [ ] GenAI, preferably offline model
+- [ ] GenAI depends on something that is currently going on (e.g. music or processes)
+- [ ] Proper README
+- [ ] Video or shaders? (check other repo)
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,53 @@
 use std::path::PathBuf;
+use std::process::Command;
+use std::fs;
+use std::thread::sleep;
+use std::time::Duration;
 use clap::Parser;
 
 // ref: https://docs.rs/clap/latest/clap/
 #[derive(Parser)]
 struct Args {
-    path: PathBuf,
-    period: u32
+    path: PathBuf
 }
 
-fn set_wallpaper(path: PathBuf) {
+fn set_wallpaper(path: &PathBuf) {
     /*
     ref: https://github.com/KDE/plasma-workspace/blob/master/wallpapers/image/plasma-apply-wallpaperimage.cpp
     plasma's own wrapper
     initial logic: just use daemon for applying wallpaper
      */
+    let script = format!("for (var key in desktops()) {{ \
+    var d = desktops()[key]; \
+    d.wallpaperPlugin = 'org.kde.image'; \
+    d.currentConfigGroup = ['Wallpaper', 'org.kde.image', 'General']; \
+    d.writeConfig('Image', 'file://{}'); \
+    d.writeConfig('FillMode', 2); }}", path.to_str().expect("Invalid path"));
 
-    println!("We got path: {:?}", path);
+    let output = Command::new("qdbus6")
+        .arg("org.kde.plasmashell")
+        .arg("/PlasmaShell")
+        .arg("org.kde.PlasmaShell.evaluateScript")
+        .arg(&script)
+        .output()
+        .expect("qdbus command failed!");
+    // println!("SCRIPT:\n {}", &script);
+
+    if !output.status.success() {
+        println!("failed to set wallpaper!");
+        println!("{:?}", output);
+    }
 }
 
 fn main() {
     // ref: https://rust-cli.github.io/book/tutorial/cli-args.html
     let args = Args::parse();
-    set_wallpaper(args.path);
-    println!("{}", args.period);
+
+    let files = fs::read_dir(args.path).unwrap();
+    for entry in files {
+        let path = &entry.unwrap().path();
+        println!("Setting wallpaper: {:?}", path);
+        set_wallpaper(path);
+        sleep(Duration::from_millis(3000));
+    }
 }
